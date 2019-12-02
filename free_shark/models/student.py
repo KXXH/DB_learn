db0b9f5a900e22b15b41796ec6457c0bd924e705
jboshu <625491845@qq.com>
2019-12-02 23:56:51 +0800

    修改增功能

--- free_shark/models/student.py
+++ free_shark/models/student.py
@@ -1,6 +1,6 @@
-from free_shark.db import get_db
+from free_shark.db import get_db,abort
 import time
-import pymysql
+
 
 class Student():
     def __init__(self,user_id,school_number,real_name,college,banji,contact):
@@ -12,6 +12,7 @@ class Student():
         self.__contact=contact
         print("到这里了！！！！！！！！！")
     
+    @staticmethod
     def add_student(user_id,school_number,real_name,college,banji,contact):
         db = get_db()
         # 使用cursor()方法获取操作游标 
@@ -19,26 +20,29 @@ class Student():
         # 创建时间
         create_time = time.strftime('%Y-%m-%d %H:%M:%S',time.localtime(time.time()))
         print(create_time)
-        sql = """INSERT INTO student(user_id,school_number,real_name,college,banji,contact,create_time) VALUES (user_id,school_number,real_name,college,banji,contact,create_time)"""
+        sql = "INSERT INTO student(user_id,school_number,real_name,college,banji,contact,create_time) VALUES (%s,%s,%s,%s,%s,%s,%s)"
         try:
             # 执行sql语句
-            cursor.execute(sql)
+            print("执行sql语句")
+            cursor.execute(sql,(user_id,school_number,real_name,college,banji,contact,create_time))
             # 提交到数据库执行
+            print("提交到数据库执行")
             db.commit()
         except:
             # 如果发生错误则回滚
             db.rollback()
+            abort(500)
         # 关闭数据库连接
         db.close()
-
+    
     def delete_student(id):
         db = get_db()
         # 使用cursor()方法获取操作游标 
         cursor = db.cursor()
-        sql = "DELETE FROM student WHERE id = %s" % (id)
+        sql = "DELETE FROM student WHERE id = %s"
         try:
             # 执行sql语句
-            cursor.execute(sql)
+            cursor.execute(sql,(id))
             # 提交到数据库执行
             db.commit()
         except:
@@ -47,28 +51,32 @@ class Student():
             # 关闭数据库连接
         db.close()
 
+   @staticmethod
+    def create_stu_from_rows(row):
+        stu=Student(user_id=row[1],school_number=row[2],real_name=row[3],college=row[3],banji=row[4],activation=row[5],contact=row[6])
+        return stu
+
+    @staticmethod
     def query_student(id):
         db = get_db()
-        # 使用cursor()方法获取操作游标 
         cursor = db.cursor()
-        sql = "SELECT FROM student WHERE id = %s" % (id)
+        #sql = "SELECT* FROM student WHERE id = %s"
         try:
             # 执行sql语句
-            cursor.execute(sql)
-            # 提交到数据库执行
-            results = cursor.fetchall()
-            for row in results:
-                user_id = row[0]
-                school_number = row[1]
-                real_name = row[2]
-                college = row[3]
-                banji = row[4]
-                contact = row[5]
-                # 打印结果
-                print ("user_id=%s,school_number=%s,real_name=%s,college=%s,banji=%s,contact=%s" % (user_id,school_number,real_name,college,banji,contact))
+            print("执行sql语句")
+            print(id)
+            cursor.execute('SELECT* FROM student WHERE id = %s',str(id))
+            print("提交到数据库执行")
+            results = cursor.fetchone()
+            if result is None:
+                return None
+            stu=Student.create_stu_from_rows(result)
+            print ("user_id=%s,school_number=%s,real_name=%s,college=%s,banji=%s,contact=%s" % (user_id,school_number,real_name,college,banji,contact))
         except:
             print ("Error: unable to fetch data")
+            abort(500)
             # 关闭数据库连接
+        return stu
         db.close()
 
     def update_student(id):
@@ -90,4 +98,4 @@ class Student():
             # 关闭数据库连接
         db.close() 
 
-        
+#Student.add_student("1","2016141441125","PDD","CS","1","buzhidao")      
